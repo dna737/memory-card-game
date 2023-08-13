a59d6076ff6ab45962b32841aa8da492906fb345
dna737 <adnanhere27@gmail.com>
2023-08-12 20:56:30 -0500

    fix bug in randomizeOrder() where random index was out of bounds

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [url, setUrl] = useState("");
+    // const [url, setUrl] = useState("");
     const [order, setOrder] = useState("");
+    console.log("order:", order);
 
     useEffect(() => {
         let ignore = false;
@@ -20,13 +21,14 @@ function App() {
                     console.log("returned value:", jsonData);
                     //TODO: go through the array of objects, set the id (they have an `ID` attribute).
                     //After that,
-                    setUrl(jsonData[1].url);
                     let arr = [];
-                    for (const object in jsonData) {
+                    for (const object of jsonData) {
                         arr.push(object.id);
+                        console.log("object:", object);
                     }
 
                     //call the randomize button here.
+                    setOrder(randomizeOrder(arr));
                     console.log("jsonData:", jsonData);
                 } catch (error) {
                     console.log("error:", error);
@@ -36,31 +38,39 @@ function App() {
 
         something();
 
+        function randomizeOrder(imageIds) {
+            // console.log(
+            // "🚀 ~ file: App.jsx:46 ~ randomizeOrder ~ imageIds):",
+            // imageIds
+            // );
+            let order = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            let newOrder = [];
+            while (newOrder.length !== order.length) {
+                let num = Math.floor(10 * Math.random());
+                if (!newOrder.includes(num)) {
+                    newOrder.push(num);
+                }
+            }
+
+            if (JSON.stringify(newOrder) === JSON.stringify(order)) {
+                return randomizeOrder();
+            }
+
+            return newOrder.map((x) => {
+                console.log("x:", x);
+                return imageIds[x];
+            });
+        }
+
         return () => {
             ignore = true;
         };
     }, []);
 
-    function randomizeOrder(imageIds) {
-        let order = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let newOrder = [];
-        while (newOrder.length !== order.length) {
-            let num = Math.floor(11 * Math.random());
-            if (!newOrder.includes(num)) {
-                newOrder.push(num);
-            }
-        }
-
-        if (JSON.stringify(newOrder) === JSON.stringify(order)) {
-            return randomizeOrder();
-        }
-
-        return newOrder.map((x) => imageIds[x]);
-    }
     return (
         <>
-            {console.log("url:", url)}
-            <img src={url}></img>
+            {/* {console.log("url:", url)} */}
+            {/* <img src={url}></img> */}
             {/* <button className="button button-primary" onClick={} */}
         </>
     );
