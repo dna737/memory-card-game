b4ed438afbeadd428de21bc20e6767d455c641f1
dna737 <adnanhere27@gmail.com>
2023-08-12 20:06:56 -0500

    remove strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+/* eslint-disable no-inner-declarations */
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [url, setUrl] = useState("");
+
+    useEffect(() => {
+        let ignore = false;
+
+        async function something() {
+            if (!ignore) {
+                console.log("rendering:");
+                try {
+                    const json = await fetch(
+                        "https://api.thecatapi.com/v1/images/search/?limit=10"
+                    );
+                    const jsonData = await json.json();
+                    console.log("returned value:", jsonData);
+                    setUrl(jsonData[1].url);
+                    console.log("jsonData:", jsonData);
+                } catch (error) {
+                    console.log("error:", error);
+                }
+            }
+        }
+
+        something();
+
+        return () => {
+            ignore = true;
+        };
+    }, []);
+
     return (
         <>
-            <button className="btn btn-info">Button</button>
+            {console.log("url:", url)}
+            <img src={url}></img>
         </>
     );
 }
