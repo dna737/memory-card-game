7e4615e2cffe4d85a5a139f2be7f28b5143dd8e7
dna737 <adnanhere27@gmail.com>
2023-08-12 20:46:26 -0500

    implement randomizeOrder()

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
     const [url, setUrl] = useState("");
+    const [order, setOrder] = useState("");
 
     useEffect(() => {
         let ignore = false;
@@ -17,7 +18,15 @@ function App() {
                     );
                     const jsonData = await json.json();
                     console.log("returned value:", jsonData);
+                    //TODO: go through the array of objects, set the id (they have an `ID` attribute).
+                    //After that,
                     setUrl(jsonData[1].url);
+                    let arr = [];
+                    for (const object in jsonData) {
+                        arr.push(object.id);
+                    }
+
+                    //call the randomize button here.
                     console.log("jsonData:", jsonData);
                 } catch (error) {
                     console.log("error:", error);
@@ -32,10 +41,27 @@ function App() {
         };
     }, []);
 
+    function randomizeOrder(imageIds) {
+        let order = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let newOrder = [];
+        while (newOrder.length !== order.length) {
+            let num = Math.floor(11 * Math.random());
+            if (!newOrder.includes(num)) {
+                newOrder.push(num);
+            }
+        }
+
+        if (JSON.stringify(newOrder) === JSON.stringify(order)) {
+            return randomizeOrder();
+        }
+
+        return newOrder.map((x) => imageIds[x]);
+    }
     return (
         <>
             {console.log("url:", url)}
             <img src={url}></img>
+            {/* <button className="button button-primary" onClick={} */}
         </>
     );
 }
